121d4529eb60ec2334412ac30e75c0365bf4223a
Kevin Collignon <kevin@kevincollignon.com>
2012-06-04 23:40:17 -0700

    Out of the box rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileWebDevelopmentRails::Application.config.secret_token = '0fff6dfa28920403db40ea533bcded51e76c1e8d3a5540d5d5c816cf511adb7d4fd0cebea99b37b4808a0244843ffa53dbc5ede42777ddbcc449815c1a44222e'
