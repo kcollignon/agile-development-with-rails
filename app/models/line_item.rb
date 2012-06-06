463df852f1f344281e427e73e7aefbbeb9385218
Kevin Collignon <kevin@kevincollignon.com>
2012-06-05 22:48:18 -0700

    Update shopping cart to reflect quantities and empty card functionality

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :product
+  attr_accessible :cart_id, :product_id, :product, :quantity
   belongs_to :cart
   belongs_to :product
+  
+  def total_price
+    product.price * quantity
+  end
+  
 end
