50a66229170f77b5f8879de22d564489c798613e
Kevin Collignon <kevin@kevincollignon.com>
2012-06-08 23:24:29 -0700

    Order notification and shipped mailers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,6 +54,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        Notifier.order_received(@order).deliver
         format.html { redirect_to store_url, notice: 'Thank you for your order!' }
         format.json { render json: @order, status: :created, location: @order }
       else
