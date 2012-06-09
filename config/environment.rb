09e731901ff4f261c9d31b2d359495170ae3f0c8
Kevin Collignon <kevin@kevincollignon.com>
2012-06-08 23:25:12 -0700

    Environment settings

--- config/environment.rb
+++ config/environment.rb
@@ -9,14 +9,14 @@ AgileWebDevelopmentRails::Application.configure do
   
 config.action_mailer.deliver_method = :smtp
 
-config.action_mailer.smtp_settings = {
-  :address  => "smtp.gmail.com",
-  :port     => 587,
-  :domain   => "kevincollignon.com",
-  :authentication => "plain",
-  :user_name => "kevin@kevincollignon.com",
-  :password => "Babykevkev1",
-  :enable_starttls_auto => true
+#config.action_mailer.smtp_settings = {
+#  :address  => "smtp.gmail.com",
+ # :port     => 587,
+ # :domain   => "",
+ # :authentication => "plain",
+ # :user_name => ",
+ # :password => "",
+ # :enable_starttls_auto => true
 }
 
 end
