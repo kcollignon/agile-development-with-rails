1510142a30f78e3a3e2e1fab8fabe802994dc210
Kevin Collignon <kevin@kevincollignon.com>
2012-06-08 22:45:37 -0700

    update gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 AgileWebDevelopmentRails::Application.initialize!
+
+
+AgileWebDevelopmentRails::Application.configure do 
+  
+config.action_mailer.deliver_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :port     => 587,
+  :domain   => "kevincollignon.com",
+  :authentication => "plain",
+  :user_name => "kevin@kevincollignon.com",
+  :password => "Babykevkev1",
+  :enable_starttls_auto => true
+}
+
+end
